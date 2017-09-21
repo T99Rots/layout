0715ebfab983f2d2687177f03bd724f8ab368b55
DESKTOP-SU164OA\Tim-Work <T99Rots@github.com>
2017-09-21 14:50:31 +0200

    to much

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function heightChange() {
+
+	document.getElementById("wrapper").style.height = (window.innerHeight + "px");
+	document.getElementById("homePage").style.height = (window.innerHeight + "px");
+
+
+}
+
+heightChange()
